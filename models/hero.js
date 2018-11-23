8f6dd01ca52f300c7938ac57acf1b9d918060553
Reshma-29 <reshmarajeev25@gmail.com>
2018-11-23 09:33:46 +0530

    First commit

--- models/hero.js
+++ models/hero.js
@@ -1,127 +1,125 @@
 var express = require('express');
-var mysql = require('mysql2');
+//var JSONData = require('./heros.json');
+var fs = require("fs");
+// get the client
+//const mysql = require('mysql2');
 
-let Heros= {}
-Heros.getAll = function(){
-	return new Promise(function(resolve,reject){
-		const connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			database: 'heros',
-			password : 'ccs#1234'
-		});
-		let query='select * from comic where is_valid = 1';
-		connection.query(query,function(err, result, fields){
-			if(err) {
-				console.log(err);
-				console.log('ERR :: fetching data from database');
-				reject();
-			}
-			else{
-			//console.log(result);
-			//console.log(fields);
-				resolve(result);
-			}
-		});
-	});
-}
+// Using Node.js `require()`
+const mongoose = require('mongoose');
 
-Heros.saveNew = function(newHeroData){
-	return new Promise(function(resolve,reject){
-		const connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			database: 'heros',
-			password : 'ccs#1234'
-		});
-		let query=`insert into comic(superhero,publisher,alter_ego,first_appearance,characters,is_valid,update_time)values('${newHeroData.superhero}','${newHeroData.publisher}','${newHeroData.alter_ego}','${newHeroData.first_appearance}','${newHeroData.characters}',1,'${new Date()}')`;
-		connection.query(query,function(err, result, fields){
-			if(err) {
-				console.log(err);
-				console.log('ERR :: fetching data from database');
-				reject();
-			}
-			else{
-			//console.log(result);
-			//console.log(fields);
-				resolve(result);
-			}
-		});
-	})
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const contact = new Schema({
+	id: ObjectId,
+	name: String,
+	phoneNo: String
+});
    
-}
+const myModel = mongoose.model('contact', contact);
 
-Heros.deleteHero= function(newHeroData){
-	return new Promise(function(resolve,reject){
-		const connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			database: 'heros',
-			password : 'ccs#1234'
-		});
-		let query=`update comic set is_valid=0 where id='${newHeroData.id}'`;
-		connection.query(query,function(err, result, fields){
+let Contact= {}
+Contact.getAll = function(){
+	return new Promise(function (resolve, reject){
+
+		// //Set up default mongoose connection
+		const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+		console.log(connection);
+		myModel.find({},function(err,contact){
 			if(err) {
 				console.log(err);
-				console.log('ERR :: fetching data from database');
+				console.log('ERR :: fetching data from database..');
 				reject();
 			}
-			else{
-			//console.log(result);
-			//console.log(fields);
-				resolve(result);
-			}
-		});
-	})
+			else {
+				//console.log(result);
+				console.log(contact);
 
-}
-Heros.updateHero= function(newHeroData){
-return new Promise(function(resolve,reject){
-		const connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			database: 'heros',
-			password : 'ccs#1234'
+				resolve(contact);
+			}
+
 		});
-		let query =`update comic set superhero= '${newHeroData.superhero}',publisher='${newHeroData.publisher}',alter_ego='${newHeroData.alter_ego}',first_appearance='${newHeroData.first_appearance}',characters='${newHeroData.characters}',is_valid=1,update_time='${new Date()}' where id='${newHeroData.id}'`;
+
+	});
 		
-		connection.query(query,function(err, result, fields){
-			if(err) {
-				console.log(err);
-				console.log('ERR :: fetching data from database');
-				reject();
-			}
-			else{
-			//console.log(result);
-			//console.log(fields);
-				resolve(result);
-				
-			}
-		});
-	})
 }
-Heros.viewHero=function(newHeroData){
-	return new Promise(function(resolve,reject){
-		const connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			database: 'heros',
-			password : 'ccs#1234'
+
+Contact.saveContact = function(newData){
+	return new Promise(function (resolve, reject){
+
+		// //Set up default mongoose connection
+		const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+		console.log(connection);
+
+
+		var saveuser = myModel({
+			name: `${newData.name}`,
+			phoneNo: `${newData.phoneNo}`
 		});
-		let query=`select * from comic where id='${newHeroData.id}'`;
-		connection.query(query,function(err, result, fields){
+		saveuser.save({},function(err,contact){
 			if(err) {
 				console.log(err);
-				console.log('ERR :: fetching data from database');
+				console.log('ERR :: fetching data from database..');
 				reject();
 			}
-			else{
-			//console.log(result);
-			//console.log(fields);
-				resolve(result);
+			else {
+				//console.log(result);
+				console.log(contact);
+				//console.log('contact saved');
+
+				resolve(contact);
 			}
+
 		});
+
 	});
 }
 
-module.exports = Heros;
+Contact.deleteContact = function(newData){
+	return new Promise(function (resolve, reject){
+
+		// //Set up default mongoose connection
+		const connection = mongoose.connect('mongodb://127.0.0.1:27017/myDB');
+		console.log(connection);
+
+
+		myModel.find({name: `${newData.name}`},function(err,contact){
+			if(err) {
+				console.log(err);
+				console.log('ERR :: fetching data from database..');
+				reject();
+			}
+			else {
+
+				//console.log(result);
+				console.log(contact);
+				//console.log('contact saved');
+
+				resolve(contact);
+
+			}
+		
+		       
+        contact.remove({},function(err,contact){
+			if(err) {
+				console.log(err);
+				console.log('ERR :: fetching data from database..');
+				reject();
+			}
+			else {
+				//console.log(result);
+				console.log(contact);
+				//console.log('contact saved');
+
+				resolve(contact);
+			}
+
+		});
+
+       
+
+	});
+});	
+}
+
+module.exports = Contact;
--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,50 @@
 var express = require('express');
 var router = express.Router();
-var Heros = require('../models/hero.js')
+
+var Contact = require('../models/hero.js')
 
 router.get('/', function(req, res, next) {
- 	res.render('index', { title: 'SuperHeros' });
+	res.render('index', { title: 'Contacts' });
 });
 
-//router.get('/saveData', function(req, res, next) {
-//	Heros.saveNew(req.query);
-//	res.render('heros',{data:req.query});
-//});
+
+router.get('/getAll', function(req, res, next) {
+	Contact.getAll()
+	.then(function(retVal){
+		console.log('here')
+		console.log(retVal)
+		res.render('contact',{data: retVal});	
+	})
+	.catch(console.log('Err: in resolving the promise'));
+  
+});
+
+
+
 router.get('/saveData', function(req, res, next) {
-	Heros.saveNew(req.query)
+	Contact.saveContact(req.query)
 	.then(function(){
-		res.redirect('/getAllHeros');
-	})
-	.catch(console.log('Err: in resolving the promise'));
-});
-router.get('/getAllHeros', function(req, res, next) {
-	Heros.getAll()
-	.then(function(retVal){
-		res.render('heros',{data: retVal});	
+		//console.log('here')
+		//console.log(retVal)
+		res.redirect('/getAll');
+		//res.render('contact',{data: retVal});	
 	})
 	.catch(console.log('Err: in resolving the promise'));
   
 });
-router.get('/deleteHero', function(req, res, next) {
-	Heros.deleteHero(req.query)
+
+
+router.get('/deleteData', function(req, res, next) {
+	Contact.deleteContact(req.query)
 	.then(function(){
-		res.redirect('/getAllHeros')
+		//console.log('here')
+		//console.log(retVal)
+		res.redirect('/getAll');
+		//res.render('contact',{data: retVal});	
 	})
 	.catch(console.log('Err: in resolving the promise'));
   
 });
-router.get('/updateData', function(req, res, next) {
-	Heros.updateHero(req.query)  
-	.then(function(retVal){
-		res.render('updatehero',{data: retVal})
-	})
-	.catch(console.log('Err: in resolving the promise'));
-  
-});
-router.get('/viewHero', function(req, res, next) {
-	Heros.viewHero(req.query)
-	.then(function(retVal){
-		res.render('viewheros',{data: retVal});	
-	})
-	.catch(console.log('Err: in resolving the promise'));
-  
-});
-
-
-
 
 
 module.exports = router;
